4cd96e3c00e715a926f68bc5108c27031b9048a1
Marilyn Du <dumf@bc.edu>
2022-12-09 12:34:08 -0500

    Initial Commit

---
+++ SimplyCooking/SimplyCookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyCookingApp.swift
+//  SimplyCooking
+//
+//  Created by Marilyn Du on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SimplyCookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
